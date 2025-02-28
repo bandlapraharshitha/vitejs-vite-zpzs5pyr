fd4f71f6854d1d22cc6b35acab8cfc64af0e6fec
bandlapraharshitha <praharshitha35@gmail.com>
2025-02-28 09:46:38 +0530

    completed assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from './components/Dashboard';
+// src/App.jsx
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import AddMovie from "./components/AddMovie";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/add-movie" element={<AddMovie />} />
       </Routes>
     </Router>
   );
